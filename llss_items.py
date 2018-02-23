284dbcf0cfd048a4f9056d609d8bc1741f1f8d85
LinHe <LinHe.Lurking@gmail.com>
2018-02-23 12:28:50 +0800

    Update llss_items.py

--- llss_items.py
+++ llss_items.py
@@ -5,6 +5,7 @@ class LlssAllItem(scrapy.Item):
     title = scrapy.Field()
     post_date = scrapy.Field()
     abstract = scrapy.Field()
-    magnet = scrapy.Field()
+    magnet_with_prefix = scrapy.Field()
+    magnet_without_prefix = scrapy.Field()
     image_urls = scrapy.Field()
     image_type = scrapy.Field()
